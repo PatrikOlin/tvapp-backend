0f4e643acf294aebc8566e29e1e8948a8af467a6
Patrik Holmkvist <patrik.holmkvist@outlook.com>
2018-11-15 13:13:38 +0100

    TVAPP-100 add swagger documenation

--- src/main/java/com/tvapp/utils/config/SecurityConfiguration.java
+++ src/main/java/com/tvapp/utils/config/SecurityConfiguration.java
@@ -21,7 +21,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable();
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and().httpBasic()
+                .and().sessionManagement().disable();
     }
 
     @Bean
